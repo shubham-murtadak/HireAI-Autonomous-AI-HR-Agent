d3acf7c80999782af722e754121302cc485acdb4
swapnilranmale <ranmale.swapnil@thinkitive.com>
2025-01-04 17:06:34 +0530

    feat:navbar work completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import CandidateLogin from "./components/CandidateLogin/CandidateLogin";
 import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/why-us" element={<div>Why Us Page</div>} />
+          <Route path="/candidate-login" element={<CandidateLogin />} />
+          <Route path="/hr-login" element={<div>HR Login Page</div>} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link
 
+import "./Navbar.css";
 function Navbar() {
   return (
-<>
-    <h1>Hello</h1>
-</>  )
+    <>
+      <div className="navbar">
+        <div className="navbar-logo">
+          <h1>Hire.ai</h1>
+        </div>
+        <div className="navbar-menus">
+          <ul>
+            <li>
+              <Link to="/why-us">Why us?</Link> {/* Use 'to' for navigation */}
+            </li>
+            <li>
+              <Link to="/candidate-login">Candidate Login</Link>
+            </li>
+            <li>
+              <Link to="/hr-login">HR Login</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
